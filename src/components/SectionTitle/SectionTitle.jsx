0ca8a7a45b2fe117587cea47d8f4ec99eaba6cf1
srabon87 <srabondas199816@gmail.com>
2025-05-15 19:19:47 +0600

    add items section done

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SectionTitle = ({ subHeading, heading }) => {
   return (
     <div className="mx-auto text-center md:w-4/12 my-8">
-      <p className="text-yellow-600 mb-2">---🔥 {subHeading} 🔥---</p>
+      <p className="text-sky-600 mb-2">---🔥 {subHeading} 🔥---</p>
       <h3 className="text-3xl text-black uppercase border-y-4 py-4">
         {heading}
       </h3>
--- src/pages/Dashboard/AddItems/AddItems.jsx
+++ src/pages/Dashboard/AddItems/AddItems.jsx
@@ -1,11 +1,90 @@
-import React from 'react';
+import React from "react";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { useForm } from "react-hook-form";
+import { FaUtensils } from "react-icons/fa";
+import { Helmet } from "react-helmet-async";
 
 const AddItems = () => {
-    return (
-        <div>
-            <h2 className="text-4xl text-center">📢ADD ITEM PAGE</h2>
-        </div>
-    );
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <div>
+      <Helmet>
+        <title>dashboard-addItems</title>
+      </Helmet>
+      <SectionTitle
+        heading="📢Add An Item"
+        subHeading="What's New"
+      ></SectionTitle>
+      <div>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="w-11/12 md:w-8/12 mx-auto space-y-4"
+        >
+          {/* Recipe Name */}
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Recipe Name*</legend>
+            <input
+              type="text"
+              {...register("name", {required: true})}
+              className="input w-full"
+              placeholder="Recipe Name"
+            />
+          </fieldset>
+
+          <div className="flex flex-col md:flex-row md:items-center gap-4 md:justify-between">
+            {/* category */}
+            <fieldset className="fieldset w-full md:w-1/2">
+              <legend className="fieldset-legend">Category*</legend>
+              <select
+                {...register("category", {required: true})}
+                defaultValue="Pick a Category"
+                className="select w-full"
+                placeholder="Category"
+              >
+                <option disabled={true}>Select a Category</option>
+                <option value="salad">Salad</option>
+                <option value="pizza">Pizza</option>
+                <option value="soup">Soup</option>
+                <option value="dessert">Dessert</option>
+                <option value="drinks">Drinks</option>
+              </select>
+            </fieldset>
+
+            {/* price */}
+            <fieldset className="fieldset w-full md:w-1/2">
+              <legend className="fieldset-legend">Price*</legend>
+              <input
+                type="text"
+                {...register("price", {required: true})}
+                className="input w-full"
+                placeholder="Price"
+              />
+            </fieldset>
+          </div>
+
+          {/* recipe details */}
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Recipe Details*</legend>
+            <textarea
+            {...register("recipe", {required: true})}
+              className="textarea h-24 w-full"
+              placeholder="Bio"
+            ></textarea>
+            <div className="label">Optional</div>
+          </fieldset>
+
+          <div>
+            <input {...register("image", {required: true})} type="file" className="file-input file-input-info" />
+          </div>
+
+          <button className="btn btn-soft btn-success">Add Item <FaUtensils></FaUtensils> </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AddItems;
